be5b726cff055a1bcf3a15e8d365dd6aa9c647dd
Suresh <nsk126@gmail.com>
2022-01-18 18:17:42 +0530

    added sin wave gen , with freq ctrl 0 to 100

--- src/Signal.js
+++ src/Signal.js
@@ -4,6 +4,13 @@ var trace1 = {
     type: 'scatter'
 };
 
+var trace2 = {
+    x: [],
+    y: [],
+    type: 'scatter'
+};
+
+
 for (let index = 0; index < 10000; index++) {
 
     // signal is A * sin ( 2 * pi * f * t)
@@ -16,3 +23,47 @@ for (let index = 0; index < 10000; index++) {
 
 var data = [trace1];
 Plotly.newPlot('myDiv', data);
+
+
+
+var slider = document.getElementById("freqRange");
+var optxt = document.getElementById("optxt1");
+
+
+
+for (let index = 0; index < 10000; index++) {
+
+        
+    // signal is A * sin ( 2 * pi * f * t)
+    var x = Math.sin(1 * 2* Math.PI * index/10000);
+    
+    trace2.y[index] = x;
+
+}
+
+trace2.x = trace1.x;
+data2 = [trace2]
+Plotly.newPlot('plot_interact', data2);
+
+slider.oninput = function(){
+    optxt.innerHTML = this.value;
+    var freq_1 = this.value;
+
+    for (let index = 0; index < 10000; index++) {
+
+        
+        // signal is A * sin ( 2 * pi * f * t)
+        var x = Math.sin(freq_1 * 2* Math.PI * index/10000);
+        
+        trace2.y[index] = x;
+    
+    }
+    trace2.x = trace1.x;
+    data2 = [trace2]
+    
+    Plotly.newPlot('plot_interact', data2);
+}
+
+
+
+
