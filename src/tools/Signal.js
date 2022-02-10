91888d4b3c668bb4c84b04818c713b6559d91246
nsk126 <nsk126@gmail.com>
2022-02-11 01:46:59 +0530

    ADDED SAW + SINE + SQR WAVE GEN

--- src/tools/Signal.js
+++ src/tools/Signal.js
@@ -10,7 +10,33 @@ var trace2 = {
     type: 'scatter'
 };
 
+let plot2_type = "sine";
 
+function swPlot(element, type_of_plot){
+    
+    if (type_of_plot == 'sine') {
+        
+        plot2_type = "sine";
+        // console.log("Sine plot");
+        update_plot(global_freq);
+        
+    }else if (type_of_plot == 'sqr') {
+        
+        plot2_type = "sqr";
+        // console.log("Sqr plot");
+        update_plot(global_freq);
+        
+    }else if (type_of_plot == 'saw') {
+        
+        plot2_type = "saw";
+        // console.log("Saw plot");
+        update_plot(global_freq);
+        
+    }
+
+}
+
+// PLOT 1 - NO NEED TO CHANGE - STATIC
 for (let index = 0; index < 10000; index++) {
 
     // signal is A * sin ( 2 * pi * f * t)
@@ -25,9 +51,12 @@ var data = [trace1];
 Plotly.newPlot('myDiv', data);
 
 
+// ---------------------------------------
 
-var slider = document.getElementById("freqRange");
-var optxt = document.getElementById("optxt1");
+
+// SLIDER BAR DOM
+let slider = document.getElementById("freqRange");
+let optxt = document.getElementById("optxt1");
 
 
 
@@ -43,18 +72,34 @@ for (let index = 0; index < 10000; index++) {
 
 trace2.x = trace1.x;
 data2 = [trace2]
+
+let global_freq = 1;
+
 Plotly.newPlot('plot_interact', data2);
 
 slider.oninput = function(){
     optxt.innerHTML = this.value;
-    var freq_1 = this.value;
+    global_freq = this.value;
 
+    update_plot(global_freq);
+}
+
+function update_plot(freq) {
     for (let index = 0; index < 10000; index++) {
 
-        
-        // signal is A * sin ( 2 * pi * f * t)
-        var x = Math.sin(freq_1 * 2* Math.PI * index/10000);
-        
+        if (plot2_type == "sine") {
+            
+            // signal is A * sin ( 2 * pi * f * t) 
+            // SINE WAVE
+            var x = Math.sin(global_freq * 2* Math.PI * index/10000);
+        }else if (plot2_type == "sqr"){
+
+            x = Math.sign(Math.sin(global_freq * 2* Math.PI * index/10000));            
+        }else if(plot2_type == "saw"){
+            
+            x = 2 * (global_freq * index/10000 % 1) - 1;
+        }
+
         trace2.y[index] = x;
     
     }
@@ -63,7 +108,4 @@ slider.oninput = function(){
     
     Plotly.newPlot('plot_interact', data2);
 }
-
-
-
 
